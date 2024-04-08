9f1b45b80f81dc57739f5a9cbe9bf501ae8bbed9
soup-bowl <code@soupbowl.io>
2024-04-08 19:29:43 +0100

    Position definable.

--- sharp/sharp.js
+++ sharp/sharp.js
@@ -8,17 +8,19 @@ module.exports = function(RED) {
 		this.width = config.width;
 		this.height = config.height;
 		this.fit = config.fit;
+		this.position = config.position;
 
 		var node = this;
 		node.on('input', function(msg) {
 			const width = parseInt(config.width);
 			const height = parseInt(config.height);
 			const fit = config.fit;
+			const position = config.position;
 
 			sharp(msg.payload)
 				.resize(width, height, {
 					fit: fit,
-					position: 'center'
+					position: position
 				})
 				.toBuffer()
 				.then(resizedImageBuffer => {
