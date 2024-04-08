ffd299c81a6957404b6da5250adf472dad1a6c25
soup-bowl <code@soupbowl.io>
2024-04-08 21:03:28 +0100

    Added Sharp conversion.

---
+++ sharp/sharp-convert.js
@@ -0,0 +1,31 @@
+module.exports = function (RED) {
+	"use strict";
+	const sharp = require("sharp");
+
+	function SharpConvertNode(config) {
+		RED.nodes.createNode(this, config);
+
+		this.format = config.format;
+
+		var node = this;
+		node.on('input', function (msg) {
+			const format = msg.sharpFormat || config.format || 'jpeg';
+
+			if (Buffer.isBuffer(msg.payload)) {
+				sharp(msg.payload)
+					.toFormat(format)
+					.toBuffer()
+					.then(resizedImageBuffer => {
+						msg.payload = resizedImageBuffer;
+						node.send(msg);
+					})
+					.catch(err => {
+						node.error('[sb-sharp] Error converting image:', err);
+					});
+			} else {
+				node.error('[sb-sharp] The payload was not an image buffer.');
+			}
+		});
+	}
+	RED.nodes.registerType("sharp-convert", SharpConvertNode);
+}
